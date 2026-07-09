5e735e07dda2d1961f3520858de1a8866837afc5
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-09 16:51:16 +0530

    Time: 0 ms (100%), Space: 20.4 MB (99.93%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int a = 0 ;
-        for(int i = 0 ; i < nums.size() ; i++)
+        for(int i =0 ; i< nums.size() ; i++)
         {
             a = a^nums[i] ;
         }
