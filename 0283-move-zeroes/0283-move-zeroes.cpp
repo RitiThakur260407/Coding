4783d95b3a559d5146a839eb65c43e886217ab7e
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-09 16:24:47 +0530

    Time: 3 ms (14.74%), Space: 23.8 MB (82.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size() ;
+        int i = 0 , j = 0 ;
+        while(i < n)
+        {
+            if(nums[i] == 0)
+            {
+                j = i ;
+                break ;
+            }
+            i++ ;
+            j++ ;
+        }
+
+        while(i < n)
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[i],nums[j]) ;
+                j++ ;
+            }
+            i++ ;
+        }
+    }
+};
