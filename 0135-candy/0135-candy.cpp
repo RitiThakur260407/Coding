3982c1026a281d1f510923967368b469a5e140a7
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-11 18:59:46 +0530

    Time: 0 ms (100%), Space: 23 MB (31.07%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size() ;
-        vector<int> left(n) , right(n) , distribute(n);
+        int n = ratings.size();
+        vector<int> curr(n);
 
-        left[0] = 1 , right[n-1] = 1 ;
-        for(int i = 1 ; i < n ; i++)
-        {
-            if(ratings[i] > ratings[i-1])
-            {
-                left[i] = left[i-1] + 1 ;
-            }
-            else
-            {
-                left[i] = 1 ;
+        curr[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                curr[i] = curr[i - 1] + 1;
+            } else {
+                curr[i] = 1;
             }
         }
 
-        for(int i = n-2 ; i >= 0 ; i--)
-        {
-            if(ratings[i] > ratings[i+1])
-            {
-                right[i] = right[i+1] + 1 ;
-            }
-            else
-            {
-                right[i] = 1 ;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                curr[i] = max(curr[i], (curr[i + 1] + 1));
+            } else {
+                curr[i] = max(curr[i], 1);
             }
         }
 
-        for(int i = 0 ; i < n ; i++)
-        {
-            distribute[i] = max(left[i],right[i]) ;
-        }
-
-        return accumulate(distribute.begin(),distribute.end(),0) ;
+        return accumulate(curr.begin(), curr.end(), 0);
     }
 };
