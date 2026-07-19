e30c53fdf89cc3d9510b17ad8a783126c29438a8
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-20 00:47:46 +0530

    Time: 6 ms (49.83%), Space: 19.1 MB (90.36%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0) ;
+        vector<vector<int>> adj(numCourses) ;
+
+        for(int i = 0 ; i < prerequisites.size() ; i++)
+        {
+            int a = prerequisites[i][0] ;
+            int b = prerequisites[i][1] ;
+
+            adj[b].push_back(a) ;
+
+            indegree[a]++ ;
+        }
+
+        queue<int> q ;
+        for(int i = 0 ; i < numCourses ; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.push(i) ;
+            }
+        }
+
+        int count = 0 ;
+
+        while(!q.empty())
+        {
+            int node = q.front() ;
+            q.pop() ;
+            count++ ;
+
+            for(auto it : adj[node])
+            {
+                indegree[it]-- ;
+                if(indegree[it] == 0)
+                {
+                    q.push(it) ;
+                }
+            }
+        }
+
+        return (count == numCourses) ;
+    }
+};
