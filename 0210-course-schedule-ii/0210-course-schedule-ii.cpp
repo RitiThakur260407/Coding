93626b0785f9dfbd670caaa7bcc66652bb9a2aa0
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-20 00:49:36 +0530

    Time: 4 ms (55.35%), Space: 18.2 MB (71.1%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,56 +1,44 @@
 class Solution {
 public:
-bool dfs(int node , vector<int>& vis , vector<int>& pathvis , stack<int>& st , vector<vector<int>>& adj)
-{
-    vis[node] = 1 ;
-    pathvis[node] = 1 ;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0), ans;
+        vector<vector<int>> adj(numCourses);
 
-    for(auto it : adj[node])
-    {
-        if(vis[it] == 0)
-        {
-            if(dfs(it,vis,pathvis,st,adj))
-            {
-                return true ;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+
+            adj[b].push_back(a);
+
+            indegree[a]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
-        else
-        {
-            if(pathvis[it] == 1)
-            return true ;
-        }
-    }
-    st.push(node) ;
-    pathvis[node] = 0 ;
-    return false ;
-}
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses) ;
-        for(int i = 0 ; i < prerequisites.size() ; i++)
-        {
-            int u = prerequisites[i][0] ;
-            int v = prerequisites[i][1] ;
-            adj[v].push_back(u) ;
-        }
-        vector<int> vis(numCourses,0) , pathvis(numCourses,0) ;
-        stack<int> st ;
 
-        for(int i = 0 ; i < numCourses ; i++)
-        {
-            if(vis[i] == 0)
-            {
-                if(dfs(i,vis,pathvis,st,adj))
-                {
-                    return {} ;
+        int count = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
                 }
             }
-        } 
-        vector<int> ans ;
-        while(!st.empty())
-        {
-            ans.push_back(st.top()) ;
-            st.pop() ;
         }
-        return ans ;
+
+        if (ans.size() == numCourses) {
+            return ans;
+        }
+
+        return {};
     }
 };
