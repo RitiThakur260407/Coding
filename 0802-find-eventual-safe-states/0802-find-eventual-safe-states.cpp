f5f5bc1b71bccf22abf30dcd636c0ed896a001bd
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-20 14:37:39 +0530

    Time: 47 ms (52.44%), Space: 66.4 MB (19.95%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-bool cycle(int node , vector<int>& ans , vector<int>& vis , vector<int>& pathvis , vector<vector<int>>& graph)
-{
-    vis[node] = 1 ;
-    pathvis[node] = 1 ;
-
-    for(auto it : graph[node])
-    {
-        if(vis[it] == 0)
-        {
-            if(cycle(it,ans,vis,pathvis,graph))
-            return true ;
-        }
-        else
-        {
-            if(pathvis[it] == 1)
-            {
-                return true ;
-            }
-        }
-    }
-    ans.push_back(node) ;
-    pathvis[node] = 0 ;
-    return false ;
-}
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size() ;
-        vector<int> ans , vis(n,0) , pathvis(n,0) ;
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(vis[i] == 0)
-            {
-                cycle(i,ans,vis,pathvis,graph) ;
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0), ans;
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            for (auto it : graph[i]) {
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
-        sort(ans.begin(),ans.end()) ;
 
-        return ans ;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        sort(ans.begin(),ans.end()) ;
+        return ans;
     }
 };
