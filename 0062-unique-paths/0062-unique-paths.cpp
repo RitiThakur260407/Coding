38da0fe447c85f36fb127d7a7228dd1bca41c9b7
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-22 12:03:57 +0530

    Time: 1 ms (26.9%), Space: 9.4 MB (30.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,7 +14,18 @@ public:
                               paths(row + 1, col, m, n, dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return paths(0, 0, m, n, dp);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        dp[m - 1][n - 1] = 1;
+
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col = n - 1; col >= 0; col--) {
+                if (row == m - 1 && col == n - 1)
+                    continue;
+
+                dp[row][col] = dp[row][col + 1] + dp[row + 1][col];
+            }
+        }
+
+        return dp[0][0];
     }
 };
