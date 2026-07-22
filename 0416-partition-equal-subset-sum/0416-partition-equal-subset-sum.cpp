ae8ec0f5dfa86fc4a64edba0eb6060aa0526a18c
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-22 20:16:02 +0530

    Time: 172 ms (20.63%), Space: 17.5 MB (59.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    bool checkSum(int ind, int target, vector<int>& arr , vector<vector<int>>& dp)
-	{
-		if (target == 0)
-			return true ;
-		
-		if (ind == 0)
-			return (target == arr[ind]) ;
-			
-		if(dp[ind][target] != -1)
-		return dp[ind][target] ;
-		
-		bool pick = false ;
-		if (target >= arr[ind])
-			{
-			pick = checkSum(ind - 1, target - arr[ind], arr,dp) ;
-		}
-		
-		bool notpick = checkSum(ind - 1, target, arr,dp) ;
-		
-		return dp[ind][target] = (pick || notpick) ;
-	}
+    bool checkSum(int ind, int target, vector<int>& arr,
+                  vector<vector<int>>& dp) {
+        if (target == 0)
+            return true;
+
+        if (ind == 0)
+            return (target == arr[ind]);
+
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
+
+        bool pick = false;
+        if (target >= arr[ind]) {
+            pick = checkSum(ind - 1, target - arr[ind], arr, dp);
+        }
+
+        bool notpick = checkSum(ind - 1, target, arr, dp);
+
+        return dp[ind][target] = (pick || notpick);
+    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
 
@@ -30,8 +29,29 @@ public:
             return false;
         }
 
-        vector<vector<int>> dp(n,vector<int>((sum/2)+1,-1)) ;
+        vector<vector<bool>> dp(n, vector<bool>((sum / 2) + 1, false));
 
-        return checkSum(n - 1, sum / 2, nums,dp);
+        for (int ind = 0; ind < n; ind++) {
+            dp[ind][0] = true;
+        }
+
+        if (nums[0] <= sum/2) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= sum / 2; target++) {
+                bool pick = false;
+                if (target >= nums[ind]) {
+                    pick = dp[ind - 1][target - nums[ind]];
+                }
+
+                bool notpick = dp[ind - 1][target];
+
+                dp[ind][target] = pick || notpick;
+            }
+        }
+
+        return dp[n - 1][sum / 2];
     }
 };
