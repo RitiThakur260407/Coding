05a9626b42b58b3faddd8d2e1c4c9f3f257f4683
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-22 08:44:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> steps(n+1) ;
-        steps[1] = 1 ;
-        steps[0] = 1 ;
+        vector<int> steps(n + 2);
+        steps[n + 1] = 0;
+        steps[n] = 1;
 
-        for(int i = 2 ; i <= n ; i++)
-        {
-            steps[i] = steps[i-1] + steps[i-2] ;
+        for (int i = n - 1; i >= 0; i--) {
+            steps[i] = steps[i + 1] + steps[i + 2];
         }
 
-        return steps[n] ;
+        return steps[0];
     }
 };
