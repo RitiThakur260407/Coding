1ac0bb0dd38b92b25ab7a5cff191d2667b918328
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-22 11:56:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.57%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int paths(int row, int col, int m, int n, vector<vector<int>>& dp) {
+        if ((row > m) || (col > n))
+            return 0;
+
+        if ((row == m - 1) && (col == n - 1))
+            return 1;
+
+        if (dp[row][col] != -1)
+            return dp[row][col];
+
+        return dp[row][col] = paths(row, col + 1, m, n, dp) +
+                              paths(row + 1, col, m, n, dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return paths(0, 0, m, n, dp);
+    }
+};
