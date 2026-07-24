11ef5a6dec9742fd0d68ce95511a84c5e26c3fb0
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-24 18:27:46 +0530

    Time: 26 ms (71.31%), Space: 26.9 MB (72.07%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int subsequences(int ind1, int ind2, string& s, string& t,
-                     vector<vector<int>>& dp) {
-        if (ind2 < 0)
-            return 1;
-
-        if (ind1 < 0)
-            return 0;
-
-        if (dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
-
-        if (s[ind1] == t[ind2]) {
-            int take = subsequences(ind1 - 1, ind2 - 1, s, t, dp);
-            int nottake = subsequences(ind1 - 1, ind2, s, t, dp);
-
-            return dp[ind1][ind2] = (take + nottake);
-        }
-
-        return dp[ind1][ind2] = subsequences(ind1 - 1, ind2, s, t, dp);
-    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<unsigned int>> dp(n+1, vector<unsigned int>(m+1));
+        for(int ind2 = 0 ; ind2 <= m ; ind2++)
+        {
+            dp[0][ind2] = 0 ;
+        }
 
-        return subsequences(n - 1, m - 1, s, t, dp);
+        for(int ind1 = 0 ; ind1 <= n ; ind1++)
+        {
+            dp[ind1][0] = 1 ;
+        }
+
+        for(int ind1 = 1 ; ind1 <= n ; ind1++)
+        {
+            for(int ind2 = 1 ; ind2 <= m ;ind2++)
+            {
+                if(s[ind1-1] == t[ind2-1])
+                {
+                    unsigned int take = dp[ind1-1][ind2-1] ;
+                    unsigned int nottake = dp[ind1-1][ind2] ;
+
+                    dp[ind1][ind2] = take + nottake ;
+                }
+                else
+                {
+                    dp[ind1][ind2] = dp[ind1-1][ind2] ;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
