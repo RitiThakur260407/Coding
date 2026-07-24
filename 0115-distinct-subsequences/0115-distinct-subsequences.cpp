998638092873edf4f81dfb2aba1feee71bbddf76
RitiThakur260407 <riti.thakur.min24@itbhu.ac.in>
2026-07-24 18:21:25 +0530

    Time: 27 ms (69.32%), Space: 27 MB (53.57%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int subsequences(int ind1, int ind2, string& s, string& t,
+                     vector<vector<int>>& dp) {
+        if (ind2 < 0)
+            return 1;
+
+        if (ind1 < 0)
+            return 0;
+
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (s[ind1] == t[ind2]) {
+            int take = subsequences(ind1 - 1, ind2 - 1, s, t, dp);
+            int nottake = subsequences(ind1 - 1, ind2, s, t, dp);
+
+            return dp[ind1][ind2] = (take + nottake);
+        }
+
+        return dp[ind1][ind2] = subsequences(ind1 - 1, ind2, s, t, dp);
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return subsequences(n - 1, m - 1, s, t, dp);
+    }
+};
